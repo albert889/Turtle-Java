7521f69cb9677d9cc0d3eaa6c8c7ed319dbe7088
Albert <92928730+albert889@users.noreply.github.com>
2021-10-31 13:08:53 +0700

    Adding project file & stdlib.jar

---
+++ Turtle/src/SimpleTurtle.java
@@ -0,0 +1,22 @@
+public class SimpleTurtle extends Turtle {
+
+    public static void main(String[] args) {
+        Turtle bob = new Turtle();
+        bob.penColor("E8ABB5");
+
+        bob.forward(120.0);
+        bob.right(60);
+        bob.forward(120.0);
+        bob.right(60);
+        bob.forward(120.0);
+        bob.right(60);
+        bob.forward(120.0);
+        bob.right(60);
+        bob.forward(120.0);
+        bob.right(60);
+        bob.forward(120.0);
+
+
+        bob.interactiveMode();
+    }
+}
